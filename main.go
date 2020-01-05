4623d925dd39fc04757cd188189f783c98448d1b
Tim Jones <tdjones74021@yahoo.com>
2020-01-04 19:31:08 -0600

    Update 'searchHandler' to call NewsAPI.org web service and 'index.html' to display parsed JSON results.

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -41,7 +44,7 @@ type Search struct {
 	SearchKey string
 	NextPage	int
 	TotalPages	int
-	Restuls	Results
+	Results	Results
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -64,8 +67,51 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 		page = "1"
 	}
 
-	fmt.Println("Search query is: ", searchKey)
-	fmt.Println("Results page is: ", page)
+	//fmt.Println("Search query is: ", searchKey)
+	//fmt.Println("Results page is: ", page)
+	search := &Search{}
+	search.SearchKey = searchKey
+
+	next, err := strconv.Atoi(page)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "*** Unexpected Server Error. Unable to parse page number.", http.StatusInternalServerError)
+		return
+	}
+
+	search.NextPage = next
+	pageSize := 20
+
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, *apiKey)
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Println("resp.StatusCode: ", resp.StatusCode)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&search.Results)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults / pageSize)))
+	err = tpl.Execute(w, search)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	
 }
 
 func main() {
