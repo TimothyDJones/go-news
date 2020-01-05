43c72b27196dc10370daa7f06bfe8cff0350c08d
Tim Jones <tdjones74021@yahoo.com>
2020-01-04 18:38:22 -0600

    Add structures for data model and handling for passing NewsAPI.org API key on command line.

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
+
+var apiKey *string
+
+type Source struct {
+	ID	interface{} `json:"id"`
+	Name	string	`json:"name"`
+}
+
+type Article struct {
+	Source	Source	`json:"source"`
+	Author	string	`json:"author"`
+	Title	string	`json:"title"`
+	Description	string	`json:"description"`
+	URL	string	`json:"url"`
+	URLToImage	string	`json:"urlToImage"`
+	PublishedAt	time.Time	`json:"publishedAt"`
+	Content	string	`json:"content"`
+}
+
+type Results struct {
+	Status	string	`json:"status"`
+	TotalResults	int	`json:"totalResults"`
+	Articles	[]Article	`json:"articles"`
+}
+
+type Search struct {
+	SearchKey string
+	NextPage	int
+	TotalPages	int
+	Restuls	Results
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 //	w.Write([]byte("<h1>Hello, World!</h1>"))
@@ -35,6 +69,14 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Read NewsAPI API key from command line
+	apiKey = flag.String("apikey", "", "NewsAPI.org API key")
+	flag.Parse()
+
+	if *apiKey == "" {
+		log.Fatal("NewsAPI.org API key must be provided.")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
